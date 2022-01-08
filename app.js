ee90612c509f76de6216b27990a023329a3267c4
Artur Mucha <arti.mucha@gmail.com>
2022-01-08 17:49:46 +0100

    base config with drawing

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const dir = path.join(__dirname, 'assets');
+const dir = path.join(__dirname, 'public');
 
+app.set("view options", {layout: false});
 app.use(express.static(dir));
+
+app.get('/', (req, res) => {
+	res.render('index.html');
+});
 
 const start = async () => {
     app.listen(5000, () => {console.log('Listening on port 5000')});
